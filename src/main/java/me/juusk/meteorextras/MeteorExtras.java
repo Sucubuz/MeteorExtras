65fe4e36d58f6048b8447e008fba1bb07f779e6d
JuusK <juliuskammeijer@gmail.com>
2024-10-25 17:28:28 +0200

    Initial commit

--- src/main/java/me/juusk/meteorextras/MeteorExtras.java
+++ src/main/java/me/juusk/meteorextras/MeteorExtras.java
@@ -3,6 +3,7 @@ package me.juusk.meteorextras;
 import me.juusk.meteorextras.modules.InfAura;
 
 import com.mojang.logging.LogUtils;
+import me.juusk.meteorextras.modules.LoginCommand;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.systems.hud.Hud;
 import meteordevelopment.meteorclient.systems.hud.HudGroup;
@@ -13,7 +14,6 @@ import org.slf4j.Logger;
 public class MeteorExtras extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
     public static final Category CATEGORY = new Category("Extras");
-    public static final HudGroup HUD_GROUP = new HudGroup("Extras");
 
     @Override
     public void onInitialize() {
@@ -21,6 +21,7 @@ public class MeteorExtras extends MeteorAddon {
 
         // Modules
         Modules.get().add(new InfAura());
+        Modules.get().add(new LoginCommand());
 
         // Commands
 
