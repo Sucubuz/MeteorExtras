f5de8bebdf93a98749e6988e91c6f6db04f73276
JuusK <juliuskammeijer@gmail.com>
2024-11-05 22:28:38 +0100

    autodrink back!!

--- src/main/java/me/juusk/meteorextras/MeteorExtras.java
+++ src/main/java/me/juusk/meteorextras/MeteorExtras.java
@@ -23,6 +23,7 @@ public class MeteorExtras extends MeteorAddon {
         Modules.get().add(new ReachPlus());
         Modules.get().add(new WGBypass());
         Modules.get().add(new AutoDrink());
+        Modules.get().add(new Prefix());
 
         // Commands
 
